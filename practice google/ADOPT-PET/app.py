614d7d1a6c226d339ee3cc2878e97d85c831422a
steven espinoza <steven@stevens-MacBook-Pro.local>
2023-02-11 16:03:51 -0500

    get-game

---
+++ practice google/ADOPT-PET/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import requests
+
+
+app = Flask(__name__)
+# api_key = 'A34F48&v=1'
+# https://api.adoptapet.com/search/pets_at_shelters?key=A34F48&v=1&output=xml&shelter_id=2342&shelter_id=17293&shelter_id=8323
+
+
+@app.route('/')
+def index():
+    response = requests.get(f'https://www.freetogame.com/api/games')
+    data = response.json()
+    return render_template('index.html', data = data) 
+
+@app.route('/message', methods=['POST'])
+def post_message():
+    
+    message = request.form['message']
+    return render_template('result.html', data = data)
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0')
