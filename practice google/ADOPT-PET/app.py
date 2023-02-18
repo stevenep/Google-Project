5577576eba5e9422a20f88d5c76b6c6f30386f07
steven espinoza <steven@stevens-MacBook-Pro.local>
2023-02-18 15:55:52 -0500

    game-search

--- practice google/ADOPT-PET/app.py
+++ practice google/ADOPT-PET/app.py
@@ -11,12 +11,22 @@ app = Flask(__name__)
 def index():
     response = requests.get(f'https://www.freetogame.com/api/games')
     data = response.json()
-    return render_template('index.html', data = data) 
+    return render_template('index.html', data = data)
+    print(data)
+
+# @app.route('/message', methods=['POST'])
+# def post_message():
+    
+#     message = request.form['message']
+#     return render_template('result.html', data = data)
 
 @app.route('/message', methods=['POST'])
 def post_message():
-    
-    message = request.form['message']
+    message = request.form['platform']
+    category = request.form['category']
+    release_date = request.form['year']
+    response = requests.get(f' https://www.freetogame.com/api/games?platform={message}&category={category}&sort-by={release_date}')
+    data = response.json()
     return render_template('result.html', data = data)
 
 if __name__== '__main__':
